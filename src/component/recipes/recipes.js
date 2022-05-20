77e6a0b0f39b5e966101d050653ca8dae2c6e2d3
kevincabreraramirez <kev.cabrera.ramirez@gmail.com>
2022-05-20 15:14:17 +0800

    indention fix

--- src/component/recipes/recipes.js
+++ src/component/recipes/recipes.js
@@ -1,25 +1,30 @@
 import React from "react";
-import { Card } from "react-bootstrap";
-import "./recipes.scss";
-import { Link } from "react-router-dom";
+import {Card} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
-const recipes = (props) => (
-  <div className="recipes">
-    <Card style={{ width: "100%" }}>
-      <Card.Img
-        variant="top"
-        src={props.image}
-        style={{ height: "200px", overflow: "hidden" }}
-      />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text>{props.description}</Card.Text>
-        <Link to={props.clicked} className="btn btn-primary">
-          View
-        </Link>
-      </Card.Body>
-    </Card>
-  </div>
-);
+const recipes = (props) => {
+  return ( 
+    <div className="mb-4">
+        <Card style={{
+            width: "100%"
+        }}>
+            <Card.Img
+                variant="top"
+                src={props.image}
+                style={{
+                height: "200px",
+                overflow: "hidden"
+            }}/>
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Text>{props.description}</Card.Text>
+                <Link to={props.clicked} className="btn btn-primary">
+                    View
+                </Link>
+            </Card.Body>
+        </Card>
+    </div>
+  );
+}
 
 export default recipes;
